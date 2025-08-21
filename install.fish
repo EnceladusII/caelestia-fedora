0cff1ce3eedf92e15384007dcbe10b9c5050e2fe
EnceladeII <wittenmeyerguillaume@proton.me>
2025-08-21 23:12:56 +0200

    Update

--- install.fish
+++ install.fish
@@ -167,7 +167,11 @@ function quickshell_install
 end
 
 function material_symbols_install --description 'Install Google Material Symbols fonts for current user'
-   sudo npm install material-symbols@latest
+   mkdir -p ~/.local/share/font
+
+   wget -O ~/.local/share/font/MaterialSymbolsRounded.ttf "https://github.com/google/material-design-icons/raw/master/variablefont/MaterialSymbolsRounded%5BFILL,GRAD,opsz,wght%5D.ttf"
+   wget -O ~/.local/share/font/MaterialSymbolsOutlined.ttf "https://github.com/google/material-design-icons/raw/master/variablefont/MaterialSymbolsOutlined%5BFILL,GRAD,opsz,wght%5D.ttf"
+   wget -O ~/.local/share/font/MaterialSymbolsSharp.ttf "https://github.com/google/material-design-icons/raw/master/variablefont/MaterialSymbolsSharp%5BFILL,GRAD,opsz,wght%5D.ttf"
 end
 
 function fonts_install
