8660ee7b80c79fcaf160d53e2a59e1d473bc2072
EnceladeII <wittenmeyerguillaume@proton.me>
2025-08-22 16:16:52 +0200

    2025-08-22 Update dots

--- install.fish
+++ install.fish
@@ -170,7 +170,7 @@ function material_symbols_install --description 'Install Google Material Symbols
    mkdir -p ~/.local/share/fonts
 
    wget -O ~/.local/share/fonts/MaterialYou/MaterialSymbolsRounded.ttf "https://github.com/google/material-design-icons/raw/master/variablefont/MaterialSymbolsRounded%5BFILL,GRAD,opsz,wght%5D.ttf"
-   wget -O ~/.local/share/fonts/MateriaYou/MaterialSymbolsOutlined.ttf "https://github.com/google/material-design-icons/raw/master/variablefont/MaterialSymbolsOutlined%5BFILL,GRAD,opsz,wght%5D.ttf"
+   wget -O ~/.local/share/fonts/MaterialYou/MaterialSymbolsOutlined.ttf "https://github.com/google/material-design-icons/raw/master/variablefont/MaterialSymbolsOutlined%5BFILL,GRAD,opsz,wght%5D.ttf"
    wget -O ~/.local/share/fonts/MaterialYou/MaterialSymbolsSharp.ttf "https://github.com/google/material-design-icons/raw/master/variablefont/MaterialSymbolsSharp%5BFILL,GRAD,opsz,wght%5D.ttf"
 end
 
