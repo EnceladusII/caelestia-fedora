11e255547e28661866458f46cb522b1d43205e0e
EnceladusII <wittenmeyerguillaume@proton.me>
2025-09-02 21:13:45 +0200

    2025-09-02 Modify fish greeting logo

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,6 +1,6 @@
 function fish_greeting
     set -l LOGO ~/.config/fastfetch/saturn.txt
-    set -l PAD 2
+    set -l PAD 2  # espace horizontal entre logo et infos
 
     if test -f $LOGO
         fastfetch --logo $LOGO --logo-padding $PAD
